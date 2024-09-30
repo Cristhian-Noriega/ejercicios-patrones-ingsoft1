74c97c68053216ffc2279dd8936dc5baafc5558b
Santiago Beroch <sberoch@gmail.com>
2024-09-30 19:30:54 -0300

    template method

---
+++ app/src/main/java/ingsoft/patrones/templateMethod/Beverage.java
@@ -0,0 +1,7 @@
+package ingsoft.patrones.templateMethod;
+
+//TODO: Implementar el patrón Strategy. NO modificar los tests.
+
+public abstract class Beverage {
+  public abstract String prepare();
+}
---
+++ app/src/test/java/ingsoft/patrones/templateMethod/BeverageTest.java
@@ -0,0 +1,46 @@
+package ingsoft.patrones.templateMethod;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BeverageTest {
+  
+  @Test
+  void testPrepareCoffee() {
+    Beverage coffee = new Coffee();
+    assertEquals(
+        "Boiling water\n" +
+        "Brewing coffee grinds\n" +
+        "Pouring into cup\n" +
+        "Adding sugar and milk\n", 
+        coffee.prepare()
+    );
+  }
+
+  @Test
+  void testPrepareTea() {
+    Beverage tea = new Tea();
+    assertEquals(
+        "Boiling water\n" +
+        "Steeping the tea\n" +
+        "Pouring into cup\n" +
+        "Adding lemon\n",
+        tea.prepare()
+    );
+  }
+
+  //TODO: Agregar la bebida 'Hot Chocolate' y probarla con el siguiente test
+  
+  // @Test
+  // void testPrepareHotChocolate() {
+  //   Beverage hotChocolate = new HotChocolate();
+  //   assertEquals(
+  //       "Boiling water\n" +
+  //       "Steeping the tea\n" +
+  //       "Pouring into cup\n" +
+  //       "Adding lemon\n",
+  //       hotChocolate.prepare()
+  //   );
+  // }
+}
