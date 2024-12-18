acdd155ddf2f82e1a44e309f32626ff20ffced06
Cristhian-Noriega <noriegacristhian57@gmail.com>
2024-12-18 13:33:26 -0300

    Builder tests modified

--- app/src/test/java/ingsoft/patrones/builder/BurguerBuilderTest.java
+++ app/src/test/java/ingsoft/patrones/builder/BurguerBuilderTest.java
@@ -5,9 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class BurguerBuilderTest {
+
+    @Test
+    public void testSimpleBurger() {
+        BurguerPart burger = new BurguerBuilder()
+                .build();
+        assertEquals("Cheese", burger.getDescription());
+        assertEquals(2, burger.getCost());
+    }
     @Test
     public void testBurguerWithBreadMeetAndCheese() {
-        BurguerPart burguer = new Bread(new Meet(new Cheese()));
+        BurguerPart burguer = new BurguerBuilder()
+                .addMeat()
+                .addBread()
+                .build();
 
         assertEquals("Bread+Cheese+Meet+Bread", burguer.getDescription());
         assertEquals(8, burguer.getCost());
